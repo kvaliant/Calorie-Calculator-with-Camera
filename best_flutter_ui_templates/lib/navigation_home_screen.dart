b7a463ee7dadae199172e717c92b9d351d68ebad
Jordan <jordanjun@yahoo.com>
2021-01-18 23:03:44 +0800

    no message

---
+++ best_flutter_ui_templates/lib/navigation_home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:best_flutter_ui_templates/app_theme.dart';
+import 'package:best_flutter_ui_templates/custom_drawer/drawer_user_controller.dart';
+import 'package:best_flutter_ui_templates/custom_drawer/home_drawer.dart';
+import 'package:best_flutter_ui_templates/feedback_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'fitness_app/fitness_app_home_screen.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget screenView;
+  DrawerIndex drawerIndex;
+
+  @override
+  void initState() {
+    drawerIndex = DrawerIndex.FeedBack;
+    screenView = FitnessAppHomeScreen();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.nearlyWhite,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            onDrawerCall: (DrawerIndex drawerIndexdata) {
+              changeIndex(drawerIndexdata);
+              //callback from drawer for replace screen as user need with passing DrawerIndex(Enum index)
+            },
+            screenView: screenView,
+            //we replace screen view as we need on navigate starting screens like MyHomePage, HelpScreen, FeedbackScreen, etc...
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      if (drawerIndex == DrawerIndex.FeedBack) {
+        setState(() {
+          screenView = FeedbackScreen();
+        });
+      } 
+    }
+  }
+}
